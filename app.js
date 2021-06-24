3f63645d10c394401649a869109322157192313d
EfCo2377 <efrat0548502377@gmail.com>
2021-06-24 18:14:54 +0300

    first-commite

---
+++ app.js
@@ -0,0 +1,28 @@
+const express=require('express')
+const app=express()
+
+const bodyParser=require('body-parser')
+app.use(bodyParser.json())
+
+const dotenv=require('dotenv')
+dotenv.config()
+
+const cors=require('cors')
+
+// const routers=require('./Router/router')
+
+// app.use(cors(),routers)
+
+const mongoose=require('mongoose')
+
+const connectParonst  = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+}
+
+mongoose.connect(process.env.DB_CONNECT,connectParonst)
+.then(()=>console.log("connect"))
+.catch((error)=>console.log(`error ${error}`))
+
+app.listen(process.env.PORT,()=>console.log("listen"))
