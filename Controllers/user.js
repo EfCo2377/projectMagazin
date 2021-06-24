b528044d10cf6b0d176ad49d63f74cbeb24f46f8
EfCo2377 <efrat0548502377@gmail.com>
2021-06-24 20:43:59 +0300

    merge-with-main

--- Controllers/user.js
+++ Controllers/user.js
@@ -1,1 +1,26 @@
- 
+const User=require('../Models/user')
+
+//create new user
+const createUser=async (req,res)=>{
+    try {
+        let user=new User(req.body)
+        await user.save()
+        res.status(200).json(user)
+    } catch (error) {
+        res.status(404).send(error)
+    }
+}
+
+//editUser
+const editUser=async (req,res)=>{
+    let {idUser}=req.params
+    let newUser=req.body
+    try {
+        let user=await User.findOneAndUpdate({_id:idUser},newUser,{new:true})
+        res.json(user)
+    } catch (error) {
+        res.status(404).send(error)
+    }
+}
+
+module.exports={createUser,editUser}
--- Router/router.js
+++ Router/router.js
@@ -1,3 +1,14 @@
 const router=require('express').Router()
+const userFunction=require('../Controllers/user')
 
-module.export=router
+//get
+
+//post
+router.post('/createUser',userFunction.createUser)
+
+//delete
+
+//put
+router.put('/editUser/:idUser',userFunction.editUser)
+
+module.exports=router
