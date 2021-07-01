ee8196ea470d6b9d7c41374c5a9a7eb0678228a4
EfCo2377 <efrat0548502377@gmail.com>
2021-07-01 19:54:09 +0300

    EC_user

--- Router/router.js
+++ Router/router.js
@@ -2,12 +2,16 @@ const router=require('express').Router()
 const userFunction=require('../Controllers/user')
 
 //get
+router.get('/getAllUsers',userFunction.getAllUsers)
+router.get('/login/:nameUser',userFunction.login)
 
 //post
 router.post('/createUser',userFunction.createUser)
 
 //delete
+router.delete('/deleteUser/:nameUser',userFunction.deleteUser)
 
 //put
-router.put('/editUser/:idUser',userFunction.editUser)
+router.put('/editUser/:nameUser',userFunction.editUser)
+
 module.exports=router
